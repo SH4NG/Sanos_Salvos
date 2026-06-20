6fb89bd9ad4260fc775f23276182eb5c1cc6911e
admin <admin1227.com>
2026-06-20 18:26:38 -0400

    feat: agregar Dockerfiles y Docker Compose

--- database/create.sql
+++ database/create.sql
@@ -1,8 +1,10 @@
+-- Bases de datos de los microservicios
 CREATE DATABASE usuarios_db;
 CREATE DATABASE mascotas_db;
 CREATE DATABASE reportes_db;
 CREATE DATABASE notificaciones_db;
 
+-- Usuario de la aplicación
 CREATE USER 'myuser'@'%' IDENTIFIED BY 'password';
 
 GRANT ALL ON usuarios_db.* TO 'myuser'@'%';
